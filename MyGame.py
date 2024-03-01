16510683a61137c4924c7a1b61a3913cd6e18fe8
Izzy Haase-Puissant <ahaasepu@uvm.edu>
2024-02-29 19:30:18 -0500

    Changed scene drawing to use a tile map
    Signed-off-by: Izzy Haase-Puissant <ahaasepu@uvm.edu>

--- MyGame.py
+++ MyGame.py
@@ -18,6 +18,8 @@ class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
+        self.tile_map = None
+
         self.scene = None
 
         self.player_sprite = None
@@ -38,15 +40,21 @@ class MyGame(arcade.Window):
     def setup(self):
 
         self.camera = arcade.Camera(self.width, self.height)
-
         self.gui_camera = arcade.Camera(self.width, self.height)
 
+        map_name = ":resources:tiled_maps/map.json"
+
+        layer_options = {
+            "Platforms": {
+                "use_spatial_hash": True,
+            },
+        }
+
+        self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING, layer_options)
+
+        self.scene = arcade.Scene.from_tilemap(self.tile_map)
+
         self.score = 0
-
-        self.scene = arcade.Scene()
-
-        self.scene.add_sprite_list("Player")
-        self.scene.add_sprite_list("Walls", use_spatial_hash=True)
 
         image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
         self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
@@ -54,29 +62,12 @@ class MyGame(arcade.Window):
         self.player_sprite.center_y = 120
         self.scene.add_sprite("Player", self.player_sprite)
 
-        for x in range(0, 1250, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
-            wall.center_x = x
-            wall.center_y = 32
-            self.scene.add_sprite("Walls", wall)
+        if self.tile_map.background_color:
+            arcade.set_background_color(self.tile_map.background_color)
 
-            coordinate_list = [[512, 96], [256, 96], [768, 96]]
-            for coordinate in coordinate_list:
-                wall = arcade.Sprite(
-                    ":resources:images/tiles/boxCrate_double.png", TILE_SCALING
-                )
-                wall.position = coordinate
-                self.scene.add_sprite("Walls", wall)
-
-            for x in range(128, 1250, 256):
-                coin = arcade.Sprite(":resources:images/items/coinGold.png", COIN_SCALING)
-                coin.center_x = x
-                coin.center_y = 96
-                self.scene.add_sprite("Coins", coin)
-
-            self.physics_engine = arcade.PhysicsEnginePlatformer(
-                self.player_sprite, gravity_constant=GRAVITY, walls=self.scene["Walls"]
-            )
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player_sprite, gravity_constant=GRAVITY, walls=self.scene["Platforms"]
+        )
 
     def on_draw(self):
         self.clear()
